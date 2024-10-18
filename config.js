80d83bbff7f4a9e8a4322c84ffe47bb32c01b202
Harry Muriithi <hharryjjack@gmail.com>
2024-10-18 11:16:24 +0300

    Updated portfolio

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const config = {
+    emailjs: {
+        publicKey: "GiZT0uTVx8DPCyyLr",
+        serviceId: "service_fodb566",
+        templateId: "template_qgom0a3"
+    }
+};
+
+export default config;
